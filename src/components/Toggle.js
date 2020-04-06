0d1667cf2d792295744b30a26e4051b5b45b4384
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2020-04-06 16:08:26 +0300

    init commit

---
+++ src/components/Toggle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Toggle = () => {
+  return (
+    <div>
+      <button>Toggle</button>
+      <p>To be Toggled</p>
+    </div>
+  );
+};
+
+export default Toggle;
